0a3e061dedda4a2cd265c3945dbbe5c22a385ac4
sgpublic <sgpublic2002@gmail.com>
2024-06-03 12:00:13 +0800

    feat: clairvoyance

--- hoshino/config_example/priconne.py
+++ hoshino/config_example/priconne.py
@@ -2,3 +2,7 @@ class arena:
     AUTH_KEY = ""
     PCRD_WASM_URL = "https://pcrdfans.com/pcrd.wasm"
     PCRD_WRAPPER_URL = "https://github.com/sgpublic/pcrd-wrapper/raw/release"
+
+class clairvoyance:
+    CLAIRVOYANCE_GIT_URL = "https://github.com/sgpublic/hoshino-clairvoyance"
+    CLAIRVOYANCE_GIT_BRANCH = "resource"
